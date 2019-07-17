8656fa6548ada507c8ff853d690afbdafccb203c
shameed97 <shameed97.pno@gmail.com>
2019-07-18 00:52:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.quiz;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME = 3000;
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        }, SPLASH_TIME);
+    }
+}
